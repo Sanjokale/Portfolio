8b1acc3cb11b7eb8af5f819c83d82dbc69cf7d62
Sanjok Alemagar <alemagarsanjok771@gmail.com>
2025-03-07 10:44:25 +0545

    footer section completed

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Approach from "@/components/Approach";
 import Blogs from "@/components/Blog";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <RecentProjects />
         <Blogs />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
